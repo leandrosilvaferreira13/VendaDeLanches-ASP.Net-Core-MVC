80ef7b59b7b87cade67ad94a9fca8fb49f788ab6
leandrosilvaferreira13 <leandro3903@gmail.com>
2023-06-18 19:51:37 -0300

    Adicionar arquivos de projeto.

---
+++ VendaDeLanches/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VendaDeLanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
