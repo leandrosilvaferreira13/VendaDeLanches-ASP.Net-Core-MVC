40c13df4673dd65277fc87c9e49b174948a27669
leandrosilvaferreira13 <leandro3903@gmail.com>
2023-06-23 17:24:21 -0300

    criando o modelo de dominio e aplicando a migrations no projeto

---
+++ VendaDeLanches/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VendaDeLanches.Models;
+
+namespace VendaDeLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+        
+
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set;}
+
+    }
+}
---
+++ VendaDeLanches/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VendaDeLanches.Models
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage ="o tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage ="Informe o nome da Categoria")]
+        [Display(Name ="Nome")]
+        public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "o tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da Categoria")]
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set; }
+    }
+}
