9e6a722f353e1b093a6b6779afc42b47f2c1cc6d
leandrosilvaferreira13 <leandro3903@gmail.com>
2023-06-26 18:51:21 -0300

    Criando o controller e View para exibir os lanches, e ajustando o código da View List

---
+++ VendaDeLanches/Controllers/LancheController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VendaDeLanches.Repositories;
+using VendaDeLanches.Repositories.Interfaces;
+
+namespace VendaDeLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+           _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewData["Titulo"] = "Todos os Lanches";
+            ViewData["Data"] = DateTime.Now;
+
+            var lanches = _lancheRepository.Lanches;
+            var totalLanches = lanches.Count();
+
+            ViewBag.Total = "Total de Lanches";
+            ViewBag.TotalLanches = totalLanches;
+            return View(lanches);
+        }
+    }
+}
